3bdbc53ecb0bf8982631220d44e103c1853246a1
hyunw9 <rkdgusdnr32@naver.com>
2024-09-21 14:18:27 +0900

    [SETTING] gitignore 디렉토리 및 파일 추가

---
+++ domain/src/main/java/sopt/makers/authentication/Main.java
@@ -0,0 +1,8 @@
+package sopt.makers.authentication;
+
+public class Main {
+
+public static void main(String[] args) {
+	System.out.println("authentication core main");
+}
+}
---
+++ domain/src/test/java/domainInitialTest.java
@@ -0,0 +1,11 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class domainInitialTest {
+
+@Test
+public void 프로젝트_초기_설정_테스트() {
+	assertThat("initial Load").isEqualTo("initial Load");
+}
+}
