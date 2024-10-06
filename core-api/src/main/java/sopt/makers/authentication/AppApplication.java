b475364851a1b0d4d093ad74e3ef2f5a60b88090
yummygyudon <bang2brew@gmail.com>
2024-10-07 01:55:44 +0900

    [CHORE] Application 모듈 등록
    - 각 모듈에 구현되어 있는 구현체들 및 Configuration들을 사용하기 위해 각 모듈의 Root 인터페이스를 BasePackageClasses로 등록해주었습니다.

--- core-api/src/main/java/sopt/makers/authentication/AppApplication.java
+++ core-api/src/main/java/sopt/makers/authentication/AppApplication.java
@@ -2,8 +2,12 @@ package sopt.makers.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sopt.makers.authentication.infra.InfraRoot;
+import sopt.makers.authentication.support.SupportRoot;
 
-@SpringBootApplication
+@SpringBootApplication(
+		scanBasePackageClasses = {DomainRoot.class, SupportRoot.class, InfraRoot.class}
+)
 public class AppApplication {
 
 	public static void main(String[] args) {
