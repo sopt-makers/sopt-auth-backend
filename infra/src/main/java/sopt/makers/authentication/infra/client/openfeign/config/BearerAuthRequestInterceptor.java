550c6ebfac715609c702a8b0efab9e723a29eb91
yummygyudon <bang2brew@gmail.com>
2024-10-08 19:54:16 +0900

    [FEAT] Playground 전용 Feign Client 및 Config 정의
    - Bearer 방식의 인증된 요청만 보낼 수 있도록 커스텀 Request Interceptor 구현하여 사용하도록 했습니다.
      - 타 클라이언트에서도 재사용 및 유연한 맞춤 구현을 할 수 abstract class로 구현했습니다.

---
+++ infra/src/main/java/sopt/makers/authentication/infra/client/openfeign/config/BearerAuthRequestInterceptor.java
@@ -0,0 +1,17 @@
+package sopt.makers.authentication.infra.client.openfeign.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.Getter;
+
+@Getter
+public abstract class BearerAuthRequestInterceptor implements RequestInterceptor {
+
+    private final String token;
+
+    public BearerAuthRequestInterceptor(String token) {
+        this.token = token;
+    }
+
+    public abstract void apply(RequestTemplate template);
+}
---
+++ infra/src/main/java/sopt/makers/authentication/infra/client/openfeign/config/PlaygroundClientConfig.java
@@ -0,0 +1,27 @@
+package sopt.makers.authentication.infra.client.openfeign.config;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import sopt.makers.authentication.infra.value.ClientConst;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class PlaygroundClientConfig {
+
+    private final ClientProperty clientValue;
+
+    @Bean
+    public RequestInterceptor playgroundRequestInterceptor() {
+        return new BearerAuthRequestInterceptor(ClientConst.AUTHORIZATION_TOKEN_PREFIX + clientValue.playground().token()) {
+            @Override
+            public void apply(RequestTemplate template) {
+                template.header(HttpHeaders.AUTHORIZATION, getToken());}
+        };
+    }
+
+}
