129f93e24776fb63bd372cbba8a890f888097f60
yummygyudon <bang2brew@gmail.com>
2024-10-08 20:10:10 +0900

    [CHORE] 각 모듈에서의 Property 클래스 활성화
    - `@EnableConfigurationProperties`를 통해 설정파일(.yml, .properties)의 값을 주입받은 Value 객체에 대해 등록하여 정상적으로 Spring Application에 등록되어 사용될 수 있도록 했습니다.

--- core-api/src/main/java/sopt/makers/authentication/AppApplication.java
+++ core-api/src/main/java/sopt/makers/authentication/AppApplication.java
@@ -2,15 +2,19 @@ package sopt.makers.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import sopt.makers.authentication.infra.InfraRoot;
+import sopt.makers.authentication.infra.client.openfeign.config.ClientProperty;
 import sopt.makers.authentication.support.SupportRoot;
+import sopt.makers.authentication.support.system.security.config.SecurityProperty;
 
 
 @SpringBootApplication(
 		scanBasePackageClasses = {DomainRoot.class, SupportRoot.class, InfraRoot.class}
 )
 @EnableFeignClients
+@EnableConfigurationProperties({ClientProperty.class, SecurityProperty.class})
 public class AppApplication {
 
 	public static void main(String[] args) {
