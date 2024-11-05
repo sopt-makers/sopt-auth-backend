f3e6a94dc4e25e1198a9d1949856da70b83604bf
yummygyudon <bang2brew@gmail.com>
2024-11-05 21:12:55 +0900

    [FEAT] Gabia 서버 통신 객체를 구현합니다.
    "SMS 문자 발송"(`#sendSmsMessage`)과 "LMS 문자 발송"(`#sendLmsMessage`) 두 가지 API에 대한 통신 메서드를 구현했습니다.
    - 각 발생 Exception 마다 try~catch를 통해 비즈니스 예외로 변환하여 던질 수 있도록 했습니다.

---
+++ src/main/java/sopt/makers/authentication/external/gabia/GabiaClient.java
@@ -0,0 +1,156 @@
+package sopt.makers.authentication.external.gabia;
+
+import sopt.makers.authentication.support.code.external.failure.ClientError;
+import sopt.makers.authentication.support.exception.external.ClientRequestException;
+import sopt.makers.authentication.support.exception.external.ClientResponseException;
+import sopt.makers.authentication.support.value.GabiaProperty;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+
+import org.springframework.stereotype.Component;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
+
+@Component
+@RequiredArgsConstructor
+class GabiaClient {
+
+  private static final String FORMAT_AUTHORIZATION = "%s:%s";
+  private static final String URI_OAUTH_TOKEN = "/oauth/token";
+  private static final String URI_SEND_SMS = "/api/send/sms";
+  private static final String URI_SEND_LMS = "/api/send/lms";
+
+  private final GabiaProperty gabiaProperty;
+
+  protected void sendSmsMessage(String receiver, String content) {
+    String authToken = authenticate();
+    OkHttpClient client = new OkHttpClient();
+
+    RequestBody requestBody =
+        new MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            // 수신번호가 두 개 이상인 경우 ',' 를 이용하여 입력 ex) 01011112222,01033334444
+            .addFormDataPart("phone", receiver)
+            .addFormDataPart("callback", gabiaProperty.sms().phone())
+            .addFormDataPart("message", content)
+            .addFormDataPart("refkey", generateReferenceKey())
+            .build();
+
+    Request request =
+        new Request.Builder()
+            .url(gabiaProperty.sms().url() + URI_SEND_SMS)
+            .post(requestBody)
+            .headers(generateHeaderOfAuthorization(encodeAuthorization(authToken)))
+            .build();
+
+    try {
+      Response response = client.newCall(request).execute();
+      String message = extractSerializedDataIn("message", response);
+      if (!message.trim().equals("Success")) {
+        ClientError error = ClientError.GABIA_RESPONSE_UNAVAILABLE;
+        error.addMessage(message);
+        throw new ClientRequestException(error);
+      }
+    } catch (IOException e) {
+      throw new ClientRequestException(ClientError.GABIA_REQUEST_INVALID_AUTH_DATA);
+    }
+  }
+
+  protected void sendLmsMessage(String receiver, String title, String content) {
+    String authToken = authenticate();
+    OkHttpClient client = new OkHttpClient();
+
+    RequestBody requestBody =
+        new MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            // 수신번호가 두 개 이상인 경우 ',' 를 이용하여 입력 ex) 01011112222,01033334444
+            .addFormDataPart("phone", receiver)
+            .addFormDataPart("callback", gabiaProperty.sms().phone())
+            .addFormDataPart("message", content)
+            .addFormDataPart("refkey", generateReferenceKey())
+            .addFormDataPart("subject", title)
+            .build();
+
+    Request request =
+        new Request.Builder()
+            .url(gabiaProperty.sms().url() + URI_SEND_LMS)
+            .post(requestBody)
+            .headers(generateHeaderOfAuthorization(encodeAuthorization(authToken)))
+            .build();
+
+    try {
+      Response response = client.newCall(request).execute();
+      String message = extractSerializedDataIn("message", response);
+      if (!message.trim().equals("Success")) {
+        ClientError error = ClientError.GABIA_RESPONSE_UNAVAILABLE;
+        error.addMessage(message);
+        throw new ClientRequestException(error);
+      }
+    } catch (IOException e) {
+      throw new ClientRequestException(ClientError.GABIA_REQUEST_INVALID_AUTH_DATA);
+    }
+  }
+
+  private String generateReferenceKey() {
+    return UUID.randomUUID().toString();
+  }
+
+  private String authenticate() {
+    OkHttpClient client = new OkHttpClient();
+    RequestBody requestBody =
+        new MultipartBody.Builder()
+            .setType(MultipartBody.FORM)
+            .addFormDataPart("grant_type", "client_credentials")
+            .build();
+
+    Request request =
+        new Request.Builder()
+            .url(gabiaProperty.sms().url() + URI_OAUTH_TOKEN)
+            .post(requestBody)
+            .headers(generateHeaderOfAuthorization(gabiaProperty.sms().key()))
+            .build();
+
+    try {
+      Response response = client.newCall(request).execute();
+      return extractSerializedDataIn("access_token", response);
+    } catch (IOException e) {
+      throw new ClientRequestException(ClientError.GABIA_REQUEST_INVALID_AUTH_DATA);
+    }
+  }
+
+  private String extractSerializedDataIn(String dataKey, Response response) {
+    try {
+      HashMap<String, String> result =
+          new Gson().fromJson(Objects.requireNonNull(response.body()).string(), HashMap.class);
+      String data = result.get(dataKey);
+      if (data == null) {
+        throw new ClientResponseException(ClientError.GABIA_RESPONSE_UNAVAILABLE);
+      }
+      return data;
+    } catch (IOException | JsonSyntaxException | JsonIOException e) {
+      throw new ClientResponseException(ClientError.GABIA_RESPONSE_BIND_FAIL);
+    }
+  }
+
+  private Headers generateHeaderOfAuthorization(String authValue) {
+    Map<String, String> headers = new HashMap<>();
+    headers.put("Content-Type", "application/x-www-form-urlencoded");
+    headers.put("Authorization", "Basic " + encodeAuthorization(authValue));
+    headers.put("cache-control", "no-cache");
+    return Headers.of(headers);
+  }
+
+  private String encodeAuthorization(String value) {
+    return Base64.getEncoder()
+        .encodeToString(
+            String.format(FORMAT_AUTHORIZATION, gabiaProperty.sms().id(), value)
+                .getBytes(StandardCharsets.UTF_8));
+  }
+}
