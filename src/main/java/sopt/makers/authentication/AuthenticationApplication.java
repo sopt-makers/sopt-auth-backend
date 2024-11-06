721c4d47c40f5ceb25ee0d59f74dbf1ff7a5fc70
sung-silver <jinna0319@gmail.com>
2024-11-06 23:21:29 +0900

    [FEAT] EnableJpaAuditing 어노테이션 추가

--- src/main/java/sopt/makers/authentication/AuthenticationApplication.java
+++ src/main/java/sopt/makers/authentication/AuthenticationApplication.java
@@ -2,8 +2,10 @@ package sopt.makers.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication(scanBasePackages = {"sopt.makers.authentication"})
+@EnableJpaAuditing
 public class AuthenticationApplication {
 
   public static void main(String[] args) {
