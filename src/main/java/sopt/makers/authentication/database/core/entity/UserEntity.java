ec7725d40bbe90cb6ef3d474cd907fbbf4b1c56d
sung-silver <jinna0319@gmail.com>
2024-11-06 22:24:43 +0900

    [FEAT] UserEntity 구현

---
+++ src/main/java/sopt/makers/authentication/database/core/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package sopt.makers.authentication.database.core.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import java.time.*;
+import lombok.*;
+import sopt.makers.authentication.database.core.common.*;
+import sopt.makers.authentication.domain.auth.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "USERS")
+public class UserEntity extends BaseTimeEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @NotNull String name;
+  @NotNull String phone;
+  @NotNull String email;
+  @NotNull LocalDate birthday;
+  @NotNull String authPlatformId;
+
+  @NotNull
+  @Enumerated(EnumType.STRING)
+  AuthPlatform authPlatformType;
+
+  @NotNull Boolean isActive;
+
+  public UserEntity(final User user, boolean isActive) {
+    Profile profile = user.getProfile();
+    SocialAccount socialAccount = user.getSocialAccount();
+
+    this.name = profile.name();
+    this.phone = profile.phone();
+    this.email = profile.email();
+    this.birthday = profile.birthday();
+    this.authPlatformId = socialAccount.authPlatformId();
+    this.authPlatformType = socialAccount.authPlatformType();
+    this.isActive = isActive;
+  }
+}
