ecb744868ef78f48d5589c252cc3624263e93493
yummygyudon <bang2brew@gmail.com>
2024-11-07 22:26:59 +0900

    [FEAT] 핸드폰 인증 도메인에 대한 데이터 처리를 담당하는 Out Port 구현
    - 파라미터로 `PhoneVerification` 도메인 객체를 주입받도록 표준화했습니다.
    - 반환 객체를 `PhoneVerification` 도메인 객체로 받도록 표준화했습니다.

---
+++ src/main/java/sopt/makers/authentication/usecase/auth/port/out/PhoneVerificationRepository.java
@@ -0,0 +1,7 @@
+package sopt.makers.authentication.usecase.auth.port.out;
+
+import sopt.makers.authentication.domain.auth.PhoneVerification;
+
+public interface PhoneVerificationRepository {
+  PhoneVerification save(PhoneVerification phoneVerification);
+}
