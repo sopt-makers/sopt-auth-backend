1ccdb46902a8032bffe2381b08b7f61ca0136cde
sung-silver <jinna0319@gmail.com>
2024-11-08 14:01:52 +0900

    [DEL] 중복 정의된 JpaEnableAuditing 제거

--- src/main/java/sopt/makers/authentication/AuthenticationApplication.java
+++ src/main/java/sopt/makers/authentication/AuthenticationApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication(scanBasePackages = {"sopt.makers.authentication"})
-@EnableJpaAuditing
 public class AuthenticationApplication {
 
   public static void main(String[] args) {
