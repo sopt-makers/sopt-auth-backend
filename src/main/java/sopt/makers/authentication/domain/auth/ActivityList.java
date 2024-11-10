0ff552630f62f77eb37d4d4cddc6b7d0b7c0f087
sung-silver <jinna0319@gmail.com>
2024-11-10 18:07:54 +0900

    [FEAT] ActivityList AllArgsConstructor 추가

--- src/main/java/sopt/makers/authentication/domain/auth/ActivityList.java
+++ src/main/java/sopt/makers/authentication/domain/auth/ActivityList.java
@@ -3,21 +3,21 @@ package sopt.makers.authentication.domain.auth;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ActivityList {
+import jakarta.validation.constraints.*;
 
+import lombok.*;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+public class ActivityList {
   private final List<Activity> activities;
 
   public ActivityList() {
     this.activities = new ArrayList<>();
   }
 
-  public ActivityList(final List<Activity> activityList) {
-    this.activities = activityList;
-  }
-
-  public ActivityList addActivity(final Activity activity) {
+  public ActivityList addActivity(@NotNull final Activity activity) {
     validateActivityDuplication(activity);
-    validateActivityEmpty(activity);
     List<Activity> updatedActivities = new ArrayList<>(this.activities);
     updatedActivities.add(activity);
     return new ActivityList(updatedActivities);
@@ -43,12 +43,5 @@ public class ActivityList {
             a -> {
               throw new IllegalArgumentException("이미 존재하는 활동 정보입니다.");
             });
-  }
-
-  private void validateActivityEmpty(final Activity activity) {
-    if (activity == null) {
-      throw new IllegalArgumentException("활동이 비어있습니다.");
-    }
-    activity.validateActivityContentsEmpty();
   }
 }
