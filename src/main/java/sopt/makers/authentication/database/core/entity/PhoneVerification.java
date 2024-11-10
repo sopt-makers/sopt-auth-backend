79e5a83d31d78969e7c6c0f748043d5ebad18bcf
sung-silver <jinna0319@gmail.com>
2024-11-10 18:35:53 +0900

    [CHORE] PhoneVerification 와일드 카드 제거

--- src/main/java/sopt/makers/authentication/database/core/entity/PhoneVerification.java
+++ src/main/java/sopt/makers/authentication/database/core/entity/PhoneVerification.java
@@ -1,6 +1,10 @@
 package sopt.makers.authentication.database.core.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "phone_verifications")
