e2dd010ff995ff7ae363e849be79a8ad371444c0
sung-silver <jinna0319@gmail.com>
2024-11-10 16:23:37 +0900

    [FEAT] SocialAccount에서 authPlatformId 및 authPlatformType에 대한 검증을 notnull 어노테이션을 이용하도록 변경

--- src/main/java/sopt/makers/authentication/domain/auth/SocialAccount.java
+++ src/main/java/sopt/makers/authentication/domain/auth/SocialAccount.java
@@ -1,19 +1,10 @@
 package sopt.makers.authentication.domain.auth;
 
-public record SocialAccount(String authPlatformId, AuthPlatform authPlatformType) {
+import jakarta.validation.constraints.*;
 
+public record SocialAccount(
+    @NotNull String authPlatformId, @NotNull AuthPlatform authPlatformType) {
   public static SocialAccount of(final String authPlatformId, final String authPlatformType) {
     return new SocialAccount(authPlatformId, AuthPlatform.find(authPlatformType));
-  }
-
-  public SocialAccount {
-    validatePlatform(authPlatformId);
-  }
-
-  // 플랫폼 정보가 비어 있는지 검증
-  private void validatePlatform(final String authPlatformId) {
-    if (authPlatformId == null || authPlatformId.isEmpty()) {
-      throw new IllegalArgumentException("빈 플랫폼 정보입니다.");
-    }
   }
 }
