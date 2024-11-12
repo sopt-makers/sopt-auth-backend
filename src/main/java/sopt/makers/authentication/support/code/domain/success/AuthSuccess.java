b8a7c6ee2c1c694a11936889dfe00bf1475765e9
yummygyudon <bang2brew@gmail.com>
2024-11-13 02:28:29 +0900

    [FEAT] "번호 인증 생성" API 구현
    - 성공에 대한 2XX 상태값의 Response 객체를 생성하기 위해 `AuthSuccess` Enum 객체를 구현했습니다.

---
+++ src/main/java/sopt/makers/authentication/support/code/domain/success/AuthSuccess.java
@@ -0,0 +1,20 @@
+package sopt.makers.authentication.support.code.domain.success;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import sopt.makers.authentication.support.code.base.SuccessCode;
+
+import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = PRIVATE)
+public enum AuthSuccess implements SuccessCode {
+  CREATE_PHONE_VERIFICATION(HttpStatus.CREATED, "번호 인증 생성에 성공했습니다."),
+  ;
+
+  private final HttpStatus status;
+  private final String message;
+}
