c41a210129218ac9708bb80569db68aa9b689d9d
yummygyudon <bang2brew@gmail.com>
2024-11-13 02:25:22 +0900

    [FEAT] 번호 인증 생성 요청 DTO 구현
    In port 내에 구현되어 있는 Command 객체로 변환하는 `toCommand()` 메서드를 선언했습니다.
    생성 요청의 경우, 요청 이력을 식별하는 ID 값이 없기 때문에 별도 long 타입 데이터는 존재하지 않습니다.
    - 인증 관심사에 대한 요청 객체 집합으로 묶어 일괄적으로 관리하기 위해 Super Class / Inner Class 형식으로 구현했습니다.
      -  상위 객체의 경우, 객체로 생성되면 안되기에 생성자를 private으로 제한했습니다.
      - 상위 객체의 경우, "실제 요청 DTO 객체를 관리"하는 Util 객체에 가까우며 상속이 되어선 안되기에 `final` 클래스로 선언했습니다.
      - 내부 객체인 "실 DTO" 객체는 필드값들의 불변성을 보장하기 위해 Record로 구현했습니다.

--- src/main/java/sopt/makers/authentication/application/auth/dto/request/AuthRequest.java
+++ src/main/java/sopt/makers/authentication/application/auth/dto/request/AuthRequest.java
@@ -2,12 +2,20 @@ package sopt.makers.authentication.application.auth.dto.request;
 
 import static lombok.AccessLevel.*;
 
+import sopt.makers.authentication.domain.auth.PhoneVerificationType;
+import sopt.makers.authentication.usecase.auth.port.in.CreatePhoneVerificationUsecase.CreateVerificationCommand;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = PRIVATE)
 public final class AuthRequest {
 
-  public record CreatePhoneVerification(String name, String number) {}
+  public record CreatePhoneVerification(String name, String number, String verificationTypeName) {
+    public CreateVerificationCommand toCommand() {
+      return new CreateVerificationCommand(
+          this.name, this.number, PhoneVerificationType.valueOf(this.verificationTypeName));
+    }
+  }
 
   public record VerifyPhoneVerification(String name, String number, String code) {}
 }
