71da50c8e74d3744502d656aa99e88892acba2be
sung-silver <jinna0319@gmail.com>
2024-11-16 00:31:38 +0900

    [FEAT] AppleProperty 구현

---
+++ src/main/java/sopt/makers/authentication/support/value/AppleProperty.java
@@ -0,0 +1,12 @@
+package sopt.makers.authentication.support.value;
+
+import org.springframework.boot.context.properties.*;
+
+@ConfigurationProperties(prefix = "external.oauth.apple")
+public record AppleProperty(Apple apple) {
+  public record Apple(String aud, String sub, Key key, Team team) {}
+
+  public record Key(String id, String path) {}
+
+  public record Team(String id) {}
+}
