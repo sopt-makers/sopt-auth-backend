4242b007734623dd9e062d3068c8da9f540015b0
sung-silver <jinna0319@gmail.com>
2024-11-17 01:50:14 +0900

    [ADD] OkHttpClientConfig에 configuration 어노테이션 추가

--- src/main/java/sopt/makers/authentication/support/config/OkHttpClientConfig.java
+++ src/main/java/sopt/makers/authentication/support/config/OkHttpClientConfig.java
@@ -1,9 +1,11 @@
 package sopt.makers.authentication.support.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import okhttp3.OkHttpClient;
 
+@Configuration
 public class OkHttpClientConfig {
   @Bean
   public OkHttpClient okHttpClient() {
