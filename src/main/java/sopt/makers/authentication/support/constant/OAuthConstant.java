cbcf2b20c6c846fdffe165fef0b9b2dcd0281085
sung-silver <jinna0319@gmail.com>
2024-11-17 22:56:38 +0900

    [FEAT] OAuthConstant에 애플 로그인 관련 상수 추가

---
+++ src/main/java/sopt/makers/authentication/support/constant/OAuthConstant.java
@@ -0,0 +1,9 @@
+package sopt.makers.authentication.support.constant;
+
+public abstract class OAuthConstant {
+  public static final String GRANT_TYPE = "authorization_code";
+  public static final String CONTENT_TYPE = "application/x-www-form-urlencoded";
+  public static final String ACCEPT = "application/json";
+  public static final int APPLE_TOKEN_EXPIRATION_TIME = 3600 * 1000; // 1 hour
+  public static final String APPLE_TOKEN_URL = "https://appleid.apple.com/auth/token";
+}
