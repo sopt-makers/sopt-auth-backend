e608d5180736082cc18bc9a578f18ca0edac586f
sung-silver <jinna0319@gmail.com>
2024-11-17 23:04:25 +0900

    [FEAT] OAuthConstant에 GOOGLE_TOKEN_URL 추가

--- src/main/java/sopt/makers/authentication/support/constant/OAuthConstant.java
+++ src/main/java/sopt/makers/authentication/support/constant/OAuthConstant.java
@@ -6,4 +6,5 @@ public abstract class OAuthConstant {
   public static final String ACCEPT = "application/json";
   public static final int APPLE_TOKEN_EXPIRATION_TIME = 3600 * 1000; // 1 hour
   public static final String APPLE_TOKEN_URL = "https://appleid.apple.com/auth/token";
+  public static final String GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token";
 }
