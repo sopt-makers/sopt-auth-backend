d8b015988b3595cf2b5b5b47955e41cc29e95b1b
yummygyudon <bang2brew@gmail.com>
2024-11-18 23:55:30 +0900

    [FEAT] 번호 인증 검사 API Response Body 데이터 정의

--- src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
+++ src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
@@ -2,7 +2,12 @@ package sopt.makers.authentication.application.auth.dto.response;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = PRIVATE)
-public final class AuthResponse {}
+public final class AuthResponse {
+
+  public record VerifyResult(@JsonProperty("isVerified") boolean isVerified) {}
+}
