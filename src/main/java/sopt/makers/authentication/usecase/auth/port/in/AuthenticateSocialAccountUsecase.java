60136f7ad9d1376f6423f126176f2e5d84b1572a
sung-silver <jinna0319@gmail.com>
2024-11-19 22:20:23 +0900

    [CHORE] 와일드 카드 제거

--- src/main/java/sopt/makers/authentication/usecase/auth/port/in/AuthenticateSocialAccountUsecase.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/port/in/AuthenticateSocialAccountUsecase.java
@@ -1,6 +1,7 @@
 package sopt.makers.authentication.usecase.auth.port.in;
 
-import sopt.makers.authentication.domain.auth.*;
+import sopt.makers.authentication.domain.auth.AuthPlatform;
+import sopt.makers.authentication.domain.auth.SocialAccount;
 
 public interface AuthenticateSocialAccountUsecase {
   SocialAccount authenticate(AuthenticateSocialAccountCommand command);
