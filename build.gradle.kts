9ba2c5b4a1b56e49a1897d86d2fc7b7c74aa6ba3
yummygyudon <bang2brew@gmail.com>
2024-11-19 14:39:44 +0900

    [CHORE] Spring Actuator 라이브러리 의존성 추가
    - Deploy 시 사용되는 Health Check API 제공
    - Prometheus 모니터링 지표 Collector 출처로 사용

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.retry:spring-retry")
 
 	implementation("com.fasterxml.jackson.core:jackson-databind:${jacksonCoreVersion}")
