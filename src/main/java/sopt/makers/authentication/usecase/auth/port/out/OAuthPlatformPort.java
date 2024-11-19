62c4a79f4e84e552023bc94c974e062b5e996132
sung-silver <jinna0319@gmail.com>
2024-11-19 22:18:06 +0900

    [CHORE] 와일드카드 제거

--- src/main/java/sopt/makers/authentication/usecase/auth/port/out/OAuthPlatformPort.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/port/out/OAuthPlatformPort.java
@@ -1,6 +1,6 @@
 package sopt.makers.authentication.usecase.auth.port.out;
 
-import sopt.makers.authentication.domain.auth.*;
+import sopt.makers.authentication.domain.auth.AuthPlatform;
 
 public interface OAuthPlatformPort {
   String getAuthPlatformId(AuthPlatform authPlatform, String code);
