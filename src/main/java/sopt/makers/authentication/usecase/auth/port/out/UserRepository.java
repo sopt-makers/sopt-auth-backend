f2a13879c765f9b7a40c43db3298528a4bf1cac1
sung-silver <jinna0319@gmail.com>
2024-11-24 05:02:32 +0900

    [CHORE] 주석 제거

--- src/main/java/sopt/makers/authentication/usecase/auth/port/out/UserRepository.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/port/out/UserRepository.java
@@ -1,7 +1,7 @@
 package sopt.makers.authentication.usecase.auth.port.out;
 
-import sopt.makers.authentication.domain.auth.*;
-import sopt.makers.authentication.domain.user.*;
+import sopt.makers.authentication.domain.auth.SocialAccount;
+import sopt.makers.authentication.domain.user.User;
 
 public interface UserRepository {
   User findBySocialAccount(SocialAccount socialAccount);
