431d12ebd5baf695fced41bdcc3cf3225bf4960e
sung-silver <jinna0319@gmail.com>
2024-11-24 00:51:58 +0900

    [REFACTOR] OAuthAuthenticator를 사용하도록 변경

--- src/main/java/sopt/makers/authentication/usecase/auth/service/AuthenticateSocialAccountService.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/AuthenticateSocialAccountService.java
@@ -2,7 +2,7 @@ package sopt.makers.authentication.usecase.auth.service;
 
 import sopt.makers.authentication.domain.auth.SocialAccount;
 import sopt.makers.authentication.usecase.auth.port.in.AuthenticateSocialAccountUsecase;
-import sopt.makers.authentication.usecase.auth.port.out.OAuthPlatformPort;
+import sopt.makers.authentication.usecase.auth.port.out.OAuthAuthenticator;
 
 import org.springframework.stereotype.Service;
 
@@ -11,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class AuthenticateSocialAccountService implements AuthenticateSocialAccountUsecase {
-  private final OAuthPlatformPort oAuthPlatformPort;
+  private final OAuthAuthenticator oAuthAuthenticator;
 
   @Override
   public SocialAccount authenticate(AuthenticateSocialAccountCommand command) {
     String authPlatformId =
-        oAuthPlatformPort.getAuthPlatformId(command.authPlatform(), command.code());
+        oAuthAuthenticator.getAuthPlatformId(command.authPlatform(), command.code());
 
     return SocialAccount.of(authPlatformId, command.authPlatform().name());
   }
