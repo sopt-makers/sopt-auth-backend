875b1d74c3e80dcaa11e251a6acb4884559e262a
sung-silver <jinna0319@gmail.com>
2024-11-24 23:29:42 +0900

    [FEAT] Web/App 소셜 로그인 응답 형식 정의

--- src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
+++ src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
@@ -6,7 +6,15 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = PRIVATE)
 public final class AuthResponse {
-  public record AuthenticateSocialAuthInfoForWeb(String accessToken) {}
+  public record AuthenticateSocialAuthInfoForWeb(String accessToken) {
+    public static AuthenticateSocialAuthInfoForWeb of(String accessToken) {
+      return new AuthenticateSocialAuthInfoForWeb(accessToken);
+    }
+  }
 
-  public record AuthenticateSocialAuthInfoForApp(String accessToken, String refreshToken) {}
+  public record AuthenticateSocialAuthInfoForApp(String accessToken, String refreshToken) {
+    public static AuthenticateSocialAuthInfoForApp of(String accessToken, String refreshToken) {
+      return new AuthenticateSocialAuthInfoForApp(accessToken, refreshToken);
+    }
+  }
 }
