6b70beeeb162401b2e59dcf4097aec9f3700d2a9
yummygyudon <bang2brew@gmail.com>
2024-12-05 02:36:01 +0900

    [REFACTOR] Usecase 결과에 대한 API Response DTO 변환 메서드 정의 및 반영

--- src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
+++ src/main/java/sopt/makers/authentication/application/auth/dto/response/AuthResponse.java
@@ -2,6 +2,8 @@ package sopt.makers.authentication.application.auth.dto.response;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import sopt.makers.authentication.usecase.auth.port.in.VerifyPhoneVerificationUsecase;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.RequiredArgsConstructor;
@@ -9,5 +11,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = PRIVATE)
 public final class AuthResponse {
 
-  public record VerifyResult(@JsonProperty("isVerified") boolean isVerified) {}
+  public record VerifyResult(@JsonProperty("isVerified") boolean isVerified) {
+    public static VerifyResult from(
+        VerifyPhoneVerificationUsecase.VerifyVerificationResult result) {
+      return new VerifyResult(result.isVerifySuccess());
+    }
+  }
 }
