4a35da4821e3e97c484744952dd14c5625f34b35
sung-silver <jinna0319@gmail.com>
2024-12-04 21:50:44 +0900

    [FEAT] SocialAccountApiController에 updateSocialAccount 구현(반환 값 제외)

---
+++ src/main/java/sopt/makers/authentication/application/auth/api/SocialAccountApiController.java
@@ -0,0 +1,27 @@
+package sopt.makers.authentication.application.auth.api;
+
+import sopt.makers.authentication.application.auth.dto.request.SocialAccountRequest;
+import sopt.makers.authentication.support.common.api.BaseResponse;
+import sopt.makers.authentication.usecase.auth.port.in.UpdateSocialAccountUsecase;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/v1/social/accounts")
+@RequiredArgsConstructor
+public class SocialAccountApiController implements SocialAccountApi {
+  private final UpdateSocialAccountUsecase updateSocialAccountUsecase;
+
+  @Override
+  @PatchMapping
+  public ResponseEntity<BaseResponse<?>> updateSocialAccount(
+      SocialAccountRequest.UpdateSocialAccount socialAccountInfo) {
+    updateSocialAccountUsecase.update(socialAccountInfo.toCommand());
+    return null;
+  }
+}
