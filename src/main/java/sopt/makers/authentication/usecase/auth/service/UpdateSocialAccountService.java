808dd8de25c56bfea1d2accc4cf76de97a06c7c4
sung-silver <jinna0319@gmail.com>
2024-12-04 21:36:29 +0900

    [FEAT] OAuthAuthenticator을 이용하여 변경된 소셜 플랫폼 id로 변경하도록 update 로직 나머지 구현

--- src/main/java/sopt/makers/authentication/usecase/auth/service/UpdateSocialAccountService.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/UpdateSocialAccountService.java
@@ -3,6 +3,7 @@ package sopt.makers.authentication.usecase.auth.service;
 import sopt.makers.authentication.domain.auth.SocialAccount;
 import sopt.makers.authentication.domain.user.User;
 import sopt.makers.authentication.usecase.auth.port.in.UpdateSocialAccountUsecase;
+import sopt.makers.authentication.usecase.auth.port.out.OAuthAuthenticator;
 import sopt.makers.authentication.usecase.auth.port.out.UserRepository;
 
 import org.springframework.stereotype.Service;
@@ -13,12 +14,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UpdateSocialAccountService implements UpdateSocialAccountUsecase {
   private final UserRepository userRepository;
+  private final OAuthAuthenticator oAuthAuthenticator;
 
   @Override
   public boolean update(UpdateSocialAccountCommand command) {
     User user = userRepository.findByPhone(command.phone());
     Long userId = userRepository.findIdByUser(user);
-    SocialAccount socialAccount = SocialAccount.of(command.code(), command.authPlatform());
+    String authPlatformId =
+        oAuthAuthenticator.getAuthPlatformId(command.authPlatform(), command.code());
+    SocialAccount socialAccount = SocialAccount.of(command.code(), authPlatformId);
     user.updateSocialAccount(socialAccount);
     userRepository.save(userId, user);
     return true;
