4dd4da32ca8eb6a8e8ecdd5fa1646d836082e317
yummygyudon <bang2brew@gmail.com>
2024-12-05 02:25:18 +0900

    [TEST] Verify API Test

--- src/main/java/sopt/makers/authentication/support/common/api/BaseResponse.java
+++ src/main/java/sopt/makers/authentication/support/common/api/BaseResponse.java
@@ -5,6 +5,8 @@ import static lombok.AccessLevel.PRIVATE;
 import sopt.makers.authentication.support.code.base.FailureCode;
 import sopt.makers.authentication.support.code.base.SuccessCode;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = PRIVATE)
 public class BaseResponse<T> {
 
+  @JsonProperty("success")
   private final boolean isSuccess;
+
+  @JsonProperty("message")
   private final String message;
+
+  @JsonProperty("data")
   private final T data;
 
   public static <T> BaseResponse<?> ofFailure(FailureCode failure, T data) {
--- src/test/java/sopt/makers/authentication/application/auth/api/AuthApiControllerTest.java
+++ src/test/java/sopt/makers/authentication/application/auth/api/AuthApiControllerTest.java
@@ -7,8 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static sopt.makers.authentication.usecase.auth.port.in.VerifyPhoneVerificationUsecase.*;
 
-import sopt.makers.authentication.application.auth.dto.request.AuthRequest;
-import sopt.makers.authentication.domain.auth.PhoneVerificationType;
 import sopt.makers.authentication.usecase.auth.port.in.CreatePhoneVerificationUsecase;
 import sopt.makers.authentication.usecase.auth.port.in.VerifyPhoneVerificationUsecase;
 
@@ -23,9 +21,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 @ContextConfiguration(classes = {AuthApiController.class})
@@ -41,58 +36,22 @@ class AuthApiControllerTest {
   @Test
   void verifyPhoneVerification() throws Exception {
     // given
-    // String name, String number, String code, String verificationTypeName
-    AuthRequest.VerifyPhoneVerification request =
-        new AuthRequest.VerifyPhoneVerification(
-            "TEST", "01012345678", "123456", PhoneVerificationType.REGISTER.name());
-    String requestBody =
+    String givenRequestBodyContent =
         "{\"name\":\"TEST\",\"phone\":\"01012345678\",\"code\":\"123456\",\"type\":\"REGISTER\"}";
-    ObjectMapper mapper = new ObjectMapper();
-    String givenRequestBody = mapper.writeValueAsString(request);
-    System.out.println(givenRequestBody);
-    //        VerifyVerificationCommand givenCommand = new VerifyVerificationCommand("TEST",
-    // "01012345678", "123456", PhoneVerificationType.REGISTER);
-    //        mock(AuthRequest.VerifyPhoneVerification.class);
-    //        when(new AuthRequest.VerifyPhoneVerification(anyString(), anyString(), anyString(),
-    // anyString()))
-    //                .thenReturn(mock(AuthRequest.VerifyPhoneVerification.class));
-    //                .thenReturn(request);
-    //        when(new VerifyVerificationCommand(anyString(), anyString(), anyString(),
-    // any(PhoneVerificationType.class)))
-    //                .thenReturn(mock(VerifyVerificationCommand.class));
-    //        String requestContent = mapper.writeValueAsString(request);
-    //
-    // doReturn(true).when(verifyPhoneVerificationUsecase.verify(any(VerifyVerificationCommand.class)));
-    //        PhoneVerificationType mockedType = mock(PhoneVerificationType.class);
-    //        AuthRequest.VerifyPhoneVerification mockedRequest =
-    // mock(AuthRequest.VerifyPhoneVerification.class);
-    //        given(mockedRequest.name()).willReturn("TEST");
-    //        given(mockedRequest.number()).willReturn("01012345678");
-    //        given(mockedRequest.code()).willReturn("123456");
-    //        given(mockedRequest.verificationTypeName()).willReturn("REGISTER");
-    //
-    //        given(mockedRequest.toCommand())
-    //                .willReturn(request.toCommand());
-
-    //
-    // when(PhoneVerificationType.getValue(anyString())).thenReturn(PhoneVerificationType.REGISTER);
-    when(verifyPhoneVerificationUsecase.verify(any(VerifyVerificationCommand.class)))
-        .thenReturn(true);
-    //        given(verifyPhoneVerificationUsecase.verify(any(VerifyVerificationCommand.class)))
-    //                .willReturn(true);
 
     // when
+    when(verifyPhoneVerificationUsecase.verify(any(VerifyVerificationCommand.class)))
+        .thenReturn(true);
     mockMvc
         .perform(
             post("/api/v1/auth/verify/phone")
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .characterEncoding("UTF-8")
-                .content(requestBody))
+                .content(givenRequestBodyContent))
         // then
         .andExpect(status().isOk())
-        .andExpect(jsonPath("$.isSuccess").value("true"))
+        .andExpect(jsonPath("$.success").value("true"))
         .andExpect(jsonPath("$.message").value("번호 인증에 성공했습니다."))
-        .andExpect(jsonPath("$.data.isVerified").value("true"))
-        .andDo(MockMvcResultHandlers.print());
+        .andExpect(jsonPath("$.data.isVerified").value("true"));
   }
 }
