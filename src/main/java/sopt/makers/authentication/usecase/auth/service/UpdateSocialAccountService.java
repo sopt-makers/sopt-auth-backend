7a1afc07caba0ac93fc42d675c189f9b4cd91433
sung-silver <jinna0319@gmail.com>
2024-12-04 21:28:24 +0900

    [FEAT] UpdateSocialAccountService에 update 메서드 구현

--- src/main/java/sopt/makers/authentication/usecase/auth/service/UpdateSocialAccountService.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/UpdateSocialAccountService.java
@@ -1,11 +1,26 @@
 package sopt.makers.authentication.usecase.auth.service;
 
-import sopt.makers.authentication.usecase.auth.port.in.*;
+import sopt.makers.authentication.domain.auth.SocialAccount;
+import sopt.makers.authentication.domain.user.User;
+import sopt.makers.authentication.usecase.auth.port.in.UpdateSocialAccountUsecase;
+import sopt.makers.authentication.usecase.auth.port.out.UserRepository;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UpdateSocialAccountService implements UpdateSocialAccountUsecase {
+  private final UserRepository userRepository;
 
   @Override
   public boolean update(UpdateSocialAccountCommand command) {
-    return false;
+    User user = userRepository.findByPhone(command.phone());
+    Long userId = userRepository.findIdByUser(user);
+    SocialAccount socialAccount = SocialAccount.of(command.code(), command.authPlatform().name());
+    user.updateSocialAccount(socialAccount);
+    userRepository.save(userId, user);
+    return true;
   }
 }
