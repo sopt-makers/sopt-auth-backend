eeaec0b4baf39b0706a42c65d789117be01b0637
sung-silver <jinna0319@gmail.com>
2024-12-04 20:50:11 +0900

    [FEAT] SocialAccountRequest에 toCommand 구현

--- src/main/java/sopt/makers/authentication/application/auth/dto/request/SocialAccountRequest.java
+++ src/main/java/sopt/makers/authentication/application/auth/dto/request/SocialAccountRequest.java
@@ -2,9 +2,16 @@ package sopt.makers.authentication.application.auth.dto.request;
 
 import static lombok.AccessLevel.PRIVATE;
 
+import sopt.makers.authentication.domain.auth.AuthPlatform;
+import sopt.makers.authentication.usecase.auth.port.in.UpdateSocialAccountUsecase.UpdateSocialAccountCommand;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = PRIVATE)
 public final class SocialAccountRequest {
-  public record UpdateSocialAccount(String phone, String code, String authPlatform) {}
+  public record UpdateSocialAccount(String phone, String code, String authPlatform) {
+    public UpdateSocialAccountCommand toCommand() {
+      return UpdateSocialAccountCommand.of(phone, AuthPlatform.find(authPlatform), code);
+    }
+  }
 }
