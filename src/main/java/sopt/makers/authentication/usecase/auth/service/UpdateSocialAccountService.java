a04ab832bbd32b47f053aa3bc050ecc654fd9d43
sung-silver <jinna0319@gmail.com>
2024-12-04 20:53:39 +0900

    [FEAT] UpdateSocialAccountService 클래스 생성

---
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/UpdateSocialAccountService.java
@@ -0,0 +1,11 @@
+package sopt.makers.authentication.usecase.auth.service;
+
+import sopt.makers.authentication.usecase.auth.port.in.*;
+
+public class UpdateSocialAccountService implements UpdateSocialAccountUsecase {
+
+  @Override
+  public boolean update(UpdateSocialAccountCommand command) {
+    return false;
+  }
+}
