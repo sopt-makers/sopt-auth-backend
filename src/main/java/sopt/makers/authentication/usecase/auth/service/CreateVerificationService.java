d6e3e29f0c08892d2f3763416c763791a3ab7ea8
yummygyudon <bang2brew@gmail.com>
2024-12-05 18:36:10 +0900

    [MODIFY] 사용자 정보가 존재할 경우에만 인증 이력이 생성되도록 기능 수정
    - `REGISTER` 타입일 경우, "동아리 가입자 정보" 데이터 베이스를 조회하도록 합니다.
    - `CHANGE`/`SEARCH` 타입의 경우, "기존 가입자 정보" 데이터 베이스를 조회하도록 합니다.

--- src/main/java/sopt/makers/authentication/usecase/auth/service/CreateVerificationService.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/CreateVerificationService.java
@@ -2,9 +2,13 @@ package sopt.makers.authentication.usecase.auth.service;
 
 import sopt.makers.authentication.domain.auth.PhoneVerification;
 import sopt.makers.authentication.domain.message.Message;
+import sopt.makers.authentication.domain.user.User;
+import sopt.makers.authentication.domain.user.UserRegisterInfo;
 import sopt.makers.authentication.usecase.auth.port.in.CreatePhoneVerificationUsecase;
 import sopt.makers.authentication.usecase.auth.port.out.PhoneVerificationRepository;
+import sopt.makers.authentication.usecase.auth.port.out.UserRepository;
 import sopt.makers.authentication.usecase.message.port.out.MessageSendPort;
+import sopt.makers.authentication.usecase.user.port.out.UserRegisterInfoRepository;
 
 import org.springframework.stereotype.Service;
 
@@ -16,13 +20,15 @@ public class CreateVerificationService implements CreatePhoneVerificationUsecase
 
   private static final String FORMAT_VERIFICATION_MESSAGE = "[SOPT makers]\n인증번호 [%s]를 입력해주세요.";
 
+  private final UserRepository userRepository;
+  private final UserRegisterInfoRepository userRegisterInfoRepository;
   private final PhoneVerificationRepository verificationRepository;
   private final MessageSendPort messageSendPort;
 
   @Override
   public PhoneVerification create(CreateVerificationCommand command) {
-    PhoneVerification phoneVerification =
-        PhoneVerification.create(command.name(), command.phone(), command.verificationType());
+    PhoneVerification phoneVerification = createPhoneVerificationByCommand(command);
+
     Message verificationMessage =
         Message.sms(
             command.phone(),
@@ -30,6 +36,21 @@ public class CreateVerificationService implements CreatePhoneVerificationUsecase
 
     messageSendPort.sendMessage(verificationMessage);
     return verificationRepository.create(phoneVerification);
+  }
+
+  private PhoneVerification createPhoneVerificationByCommand(CreateVerificationCommand command) {
+    return switch (command.verificationType()) {
+      case REGISTER -> {
+        UserRegisterInfo registerInfo = userRegisterInfoRepository.findByPhone(command.phone());
+        yield PhoneVerification.create(
+            registerInfo.getName(), registerInfo.getPhone(), command.verificationType());
+      }
+      case CHANGE, SEARCH -> {
+        User user = userRepository.findByPhone(command.phone());
+        yield PhoneVerification.create(
+            user.getProfile().name(), user.getProfile().phone(), command.verificationType());
+      }
+    };
   }
 
   private String convertCodeToMessage(String code) {
