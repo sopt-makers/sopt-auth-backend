6a5a6518f287c524b934d50b127d0dc1addd454b
yummygyudon <bang2brew@gmail.com>
2024-12-05 18:46:00 +0900

    [MODIFY] 인증 처리 Usecase Return 방식 VO 반환 방식으로의 변경에 따른 반영
    - 구현체 VerifyVerificationServiceTest 내 코드 반영까지 완료했습니다.

--- src/test/java/sopt/makers/authentication/usecase/auth/service/VerifyVerificationServiceTest.java
+++ src/test/java/sopt/makers/authentication/usecase/auth/service/VerifyVerificationServiceTest.java
@@ -45,7 +45,7 @@ class VerifyVerificationServiceTest {
     VerifyVerificationResult result = verifyService.verify(givenCommand);
 
     // then
-    assertThat(result.isVerifySuccess()).isTrue();
+    assertThat(result.isSuccess()).isTrue();
     assertThatThrownBy(() -> phoneVerificationRepository.findByPhoneVerification(givenVerification))
         .isInstanceOf(AuthException.class)
         .hasMessageContaining(AuthFailure.NOT_FOUND_PHONE_VERIFICATION.getMessage());
