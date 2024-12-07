802b8837c6e5c305f864016e00e25a4950ec9b49
hyunw9 <rkdgusdnr32@naver.com>
2024-12-08 00:04:51 +0900

    [CHORE] Spotless Apply

--- src/main/java/sopt/makers/authentication/support/constant/JwtConstant.java
+++ src/main/java/sopt/makers/authentication/support/constant/JwtConstant.java
@@ -5,5 +5,4 @@ public class JwtConstant {
   public static final String TOKEN_HEADER = "Bearer ";
   public static final String[] SERVICE_NAMES = {"playground", "crew", "app", "admin"};
   public static final String REFRESH_TOKEN_HEADER = "refresh-token";
-
 }
