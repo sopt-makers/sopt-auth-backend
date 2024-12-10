8e38d882207882efdfe9ff2a16231284b5c8b2a0
yummygyudon <bang2brew@gmail.com>
2024-12-10 21:21:16 +0900

    [CHORE] import 명시화 (`SocialAuthApiController`)
    - [참고리뷰](https://github.com/sopt-makers/sopt-auth-backend/pull/27#discussion_r1876522139)

--- src/main/java/sopt/makers/authentication/application/auth/api/SocialAccountApiController.java
+++ src/main/java/sopt/makers/authentication/application/auth/api/SocialAccountApiController.java
@@ -9,7 +9,11 @@ import sopt.makers.authentication.usecase.auth.port.in.GetSocialAccountUsecase;
 import sopt.makers.authentication.usecase.auth.port.in.UpdateSocialAccountUsecase;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
