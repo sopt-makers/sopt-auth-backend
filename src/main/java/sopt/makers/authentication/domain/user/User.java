c9213cad449952f267f8450a9519fd7975e2add9
sung-silver <jinna0319@gmail.com>
2025-03-27 01:05:01 +0900

    [REFACTOR] User 도메인에서 updateSocialAccount를 void를 반환하도록 변경

--- src/main/java/sopt/makers/authentication/domain/user/User.java
+++ src/main/java/sopt/makers/authentication/domain/user/User.java
@@ -34,8 +34,8 @@ public class User {
         .build();
   }
 
-  public User updateSocialAccount(final SocialAccount socialAccount) {
-    return User.builder()
+  public void updateSocialAccount(final SocialAccount socialAccount) {
+    User.builder()
         .id(this.id)
         .socialAccount(socialAccount)
         .profile(this.profile)
