dea8b7aed5537334b0f4d33011798ec130d2d0c7
정윤 <sju08227@naver.com>
2025-03-29 17:48:55 +0900

    [REFACTOR] UserActivityHistoryRegister @transactional 제거

--- src/main/java/sopt/makers/authentication/database/rdb/repository/user/activity/UserActivityHistoryRegister.java
+++ src/main/java/sopt/makers/authentication/database/rdb/repository/user/activity/UserActivityHistoryRegister.java
@@ -3,12 +3,10 @@ package sopt.makers.authentication.database.rdb.repository.user.activity;
 import sopt.makers.authentication.database.rdb.entity.UserActivityHistoryEntity;
 
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Component
-@Transactional
 @RequiredArgsConstructor
 public class UserActivityHistoryRegister {
 
