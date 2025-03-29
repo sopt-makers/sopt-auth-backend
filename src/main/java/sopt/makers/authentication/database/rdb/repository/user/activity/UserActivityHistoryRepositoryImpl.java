e438bce72b393d867db6af889aa92625014cec60
정윤 <sju08227@naver.com>
2025-03-29 17:51:11 +0900

    [REFACTOR] UserActivityHistoryRepositoryImpl @transactional 추가
    - 성능상 이점을 위해, 클래스단에는 읽기전용 트랜잭션을 적용했습니다

--- src/main/java/sopt/makers/authentication/database/rdb/repository/user/activity/UserActivityHistoryRepositoryImpl.java
+++ src/main/java/sopt/makers/authentication/database/rdb/repository/user/activity/UserActivityHistoryRepositoryImpl.java
@@ -6,15 +6,18 @@ import sopt.makers.authentication.domain.user.User;
 import sopt.makers.authentication.usecase.user.port.out.UserActivityHistoryRepository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 @Repository
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserActivityHistoryRepositoryImpl implements UserActivityHistoryRepository {
 
   private final UserActivityHistoryRegister userActivityHistoryRegister;
 
+  @Transactional
   @Override
   public void save(User user, Activity activity) {
     UserActivityHistoryEntity userActivityHistoryEntity =
