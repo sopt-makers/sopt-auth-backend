7e6d1c3f24adef187229c47c5ac7616b15ab5690
sung-silver <jinna0319@gmail.com>
2025-03-30 11:12:13 +0900

    [FEAT] CommonFailure에 INVALID_API_KEY 예외코드 정의

--- src/main/java/sopt/makers/authentication/support/code/support/failure/CommonFailure.java
+++ src/main/java/sopt/makers/authentication/support/code/support/failure/CommonFailure.java
@@ -11,6 +11,7 @@ import lombok.*;
 @Getter
 @RequiredArgsConstructor(access = PRIVATE)
 public enum CommonFailure implements FailureCode {
+  INVALID_API_KEY(HttpStatus.FORBIDDEN, "API 키가 유효하지 않습니다"),
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다");
   private final HttpStatus status;
   private final String message;
