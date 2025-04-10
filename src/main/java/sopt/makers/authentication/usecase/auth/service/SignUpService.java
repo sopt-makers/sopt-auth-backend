db31c2fc11b8db2e6de3727ab4507f23d4ff173b
sung-silver <jinna0319@gmail.com>
2025-04-10 13:58:31 +0900

    [FIX] SignUp - registerinfo 조회 시 optional로 조회하는 로직 반영

--- src/main/java/sopt/makers/authentication/usecase/auth/service/SignUpService.java
+++ src/main/java/sopt/makers/authentication/usecase/auth/service/SignUpService.java
@@ -1,16 +1,21 @@
 package sopt.makers.authentication.usecase.auth.service;
 
+import static sopt.makers.authentication.support.code.domain.failure.AuthFailure.NOT_FOUND_REGISTER_INFO;
+
 import sopt.makers.authentication.domain.auth.AuthPlatform;
 import sopt.makers.authentication.domain.auth.SocialAccount;
 import sopt.makers.authentication.domain.user.Activity;
 import sopt.makers.authentication.domain.user.Profile;
 import sopt.makers.authentication.domain.user.User;
 import sopt.makers.authentication.domain.user.UserRegisterInfo;
+import sopt.makers.authentication.support.exception.domain.AuthException;
 import sopt.makers.authentication.usecase.auth.port.in.SignUpUsecase;
 import sopt.makers.authentication.usecase.auth.port.out.OAuthAuthenticator;
 import sopt.makers.authentication.usecase.auth.port.out.UserRepository;
 import sopt.makers.authentication.usecase.user.port.out.UserActivityHistoryRepository;
 import sopt.makers.authentication.usecase.user.port.out.UserRegisterInfoRepository;
+
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -31,14 +36,20 @@ public class SignUpService implements SignUpUsecase {
   public void signUp(SignUpCommand command) {
     String authPlatformId =
         oAuthAuthenticator.getIdentifier(command.token(), command.authPlatform());
-    UserRegisterInfo registerInfo = userRegisterInfoRepository.findByPhone(command.phone());
+    Optional<UserRegisterInfo> targetRegisterInfo =
+        userRegisterInfoRepository.findByPhone(command.phone());
 
+    if (targetRegisterInfo.isEmpty()) {
+      throw new AuthException(NOT_FOUND_REGISTER_INFO);
+    }
+
+    UserRegisterInfo registerInfo = targetRegisterInfo.get();
     SocialAccount socialAccount = createSocialAccount(authPlatformId, command.authPlatform());
     Profile profile = createProfile(registerInfo);
     Activity activity = createActivity(registerInfo);
     User newUser = User.createNewUser(socialAccount, profile);
-
     User savedUser = userRepository.save(newUser);
+
     userActivityHistoryRepository.save(savedUser, activity);
     userRegisterInfoRepository.delete(registerInfo);
   }
