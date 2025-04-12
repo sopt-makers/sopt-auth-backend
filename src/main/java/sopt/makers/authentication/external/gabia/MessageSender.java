c79172cac312f0cfa72430a41dd0f73c168fe0cc
seoyeonjiin <duswls214@khu.ac.kr>
2025-04-12 20:19:14 +0900

    [FEAT] 문자 인증 비동기 처리

--- src/main/java/sopt/makers/authentication/external/gabia/MessageSender.java
+++ src/main/java/sopt/makers/authentication/external/gabia/MessageSender.java
@@ -3,6 +3,7 @@ package sopt.makers.authentication.external.gabia;
 import sopt.makers.authentication.domain.message.Message;
 import sopt.makers.authentication.usecase.message.port.out.MessageSendPort;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -14,6 +15,7 @@ public class MessageSender implements MessageSendPort {
   private final GabiaClient gabiaClient;
 
   @Override
+  @Async
   public void sendMessage(Message message) {
     switch (message.getType()) {
       case SMS -> gabiaClient.sendSmsMessage(message.getReceiver(), message.getContent());
