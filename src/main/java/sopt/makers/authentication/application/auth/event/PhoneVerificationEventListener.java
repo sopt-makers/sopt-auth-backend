f957d76327b002bc2dd299244a15d4069dbba7dd
sung-silver <jinna0319@gmail.com>
2025-04-16 02:48:26 +0900

    [FIX] EventListener에서 비즈니스 로직 삭제

--- src/main/java/sopt/makers/authentication/application/auth/event/PhoneVerificationEventListener.java
+++ src/main/java/sopt/makers/authentication/application/auth/event/PhoneVerificationEventListener.java
@@ -14,17 +14,12 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class PhoneVerificationEventListener {
-
-  private static final String FORMAT_VERIFICATION_MESSAGE = "[SOPT makers]\n인증번호 [%s]를 입력해주세요.";
-
   private final MessageSendPort messageSendPort;
 
   @Async
   @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void handlePhoneVerificationCreated(PhoneVerificationCreatedEvent event) {
-    String content = String.format(FORMAT_VERIFICATION_MESSAGE, event.code());
-
-    Message message = Message.sms(event.phone(), content);
+    Message message = Message.sms(event.phone(), event.content());
 
     messageSendPort.sendMessage(message);
   }
