95255d4e53530bf5115f9a8fa2800dee95f80dfe
정윤 <sju08227@naver.com>
2025-05-03 03:49:49 +0900

    [FEAT] userId 검증을 위한 UserIdValidator 구햔
    - userId 값이 1이상인지를 검증하기 위해 구현했습니다.

--- src/main/java/sopt/makers/authentication/support/code/domain/failure/UserFailure.java
+++ src/main/java/sopt/makers/authentication/support/code/domain/failure/UserFailure.java
@@ -15,6 +15,7 @@ public enum UserFailure implements FailureCode {
   // 400
   DUPLICATE_ACTIVITY(HttpStatus.BAD_REQUEST, "이미 존재하는 활동 정보입니다"),
   ROLE_REQUIRES_PART(HttpStatus.BAD_REQUEST, "해당 Role은 Part가 필수입니다"),
+  NOT_VALID_USER_ID(HttpStatus.BAD_REQUEST, "유저 ID는 1 이상이어야 합니다."),
   // 404
   NOT_FOUND_ROLE(HttpStatus.NOT_FOUND, "존재하지 않는 역할입니다"),
   NOT_FOUND_PART(HttpStatus.NOT_FOUND, "존재하지 않는 파트입니다"),
---
+++ src/main/java/sopt/makers/authentication/support/validator/UserIdValidator.java
@@ -0,0 +1,19 @@
+package sopt.makers.authentication.support.validator;
+
+import static sopt.makers.authentication.support.code.domain.failure.UserFailure.NOT_VALID_USER_ID;
+
+import sopt.makers.authentication.support.exception.domain.UserException;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserIdValidator {
+
+  public void validateUserIds(List<Long> userIds) {
+    if (userIds.stream().anyMatch(id -> id <= 0)) {
+      throw new UserException(NOT_VALID_USER_ID);
+    }
+  }
+}
