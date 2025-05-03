6689c91bd044a16312c91bcb32847324aa5f8862
정윤 <sju08227@naver.com>
2025-05-03 15:32:01 +0900

    [FEAT] Activity 도메인 생성시 userId도 함께 생성하도록 메서드 추가

--- src/main/java/sopt/makers/authentication/database/rdb/entity/UserActivityHistoryEntity.java
+++ src/main/java/sopt/makers/authentication/database/rdb/entity/UserActivityHistoryEntity.java
@@ -91,4 +91,8 @@ public class UserActivityHistoryEntity {
   public Activity toDomain() {
     return Activity.of(id, generation, team, part, role);
   }
+
+  public Activity toDomainWithUser() {
+    return Activity.of(id, user.toDomain().getId(), generation, team, part, role);
+  }
 }
