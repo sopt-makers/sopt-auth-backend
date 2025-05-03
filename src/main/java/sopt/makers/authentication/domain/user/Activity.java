98922509a1f9ed651fd6bc554911ebccf017048b
정윤 <sju08227@naver.com>
2025-05-03 15:28:20 +0900

    [FEAT] Activity 도메인 생성시 userId도 포함하도록 정적팩토리 메서드 오버로딩

--- src/main/java/sopt/makers/authentication/domain/user/Activity.java
+++ src/main/java/sopt/makers/authentication/domain/user/Activity.java
@@ -11,30 +11,36 @@ import lombok.Getter;
 @Getter
 public class Activity {
   private Long id;
+  private Long userId;
   private final int generation;
   private final Team team;
   private final Part part;
   private final Role role;
 
-  private Activity(Long id, int generation, Team team, Part part, Role role) {
+  private Activity(Long id, Long userId, int generation, Team team, Part part, Role role) {
     this.id = id;
     this.generation = generation;
+    this.userId = userId;
     this.team = team;
     this.part = part;
     this.role = role;
   }
 
   public static Activity of(int generation, final Team team, final Part part) {
-    return new Activity(null, generation, team, part, Role.MEMBER);
+    return new Activity(null, null, generation, team, part, Role.MEMBER);
   }
 
   public static Activity of(int generation, final Team team, final Part part, final Role role) {
-    return new Activity(null, generation, team, part, role);
+    return new Activity(null, null, generation, team, part, role);
   }
 
   public static Activity of(
       Long id, int generation, final Team team, final Part part, final Role role) {
-    return new Activity(id, generation, team, part, role);
+    return new Activity(id, null, generation, team, part, role);
+  }
+
+  public static Activity of(Long id, Long userId, int generation, Team team, Part part, Role role) {
+    return new Activity(id, userId, generation, team, part, role);
   }
 
   public Optional<Team> optionalTeam() {
