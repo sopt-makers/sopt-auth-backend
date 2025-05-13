8630646b0aed5008624fad7e1ad5d7e909166b19
정윤 <sju08227@naver.com>
2025-05-13 16:31:52 +0900

    [FEAT] 유저 정보 수정 Controller 구현

--- src/main/java/sopt/makers/authentication/application/user/api/UserApi.java
+++ src/main/java/sopt/makers/authentication/application/user/api/UserApi.java
@@ -3,17 +3,28 @@ package sopt.makers.authentication.application.user.api;
 import static sopt.makers.authentication.support.constant.SystemConstant.API_KEY_HEADER;
 import static sopt.makers.authentication.support.constant.SystemConstant.SERVICE_NAME_HEADER;
 
+import sopt.makers.authentication.application.user.dto.request.UserRequest;
 import sopt.makers.authentication.support.common.api.BaseResponse;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface UserApi {
-  ResponseEntity<BaseResponse<?>> getUserInformation(
+  ResponseEntity<BaseResponse<?>> getUserProfile(
       @RequestHeader(API_KEY_HEADER) String apiKey,
       @RequestHeader(SERVICE_NAME_HEADER) String serviceName,
       @RequestParam List<Long> userIds);
+
+  ResponseEntity<BaseResponse<?>> updateUserProfile(
+      @RequestHeader(API_KEY_HEADER) String apiKey,
+      @RequestHeader(SERVICE_NAME_HEADER) String serviceName,
+      @PathVariable Long userId,
+      @Valid @RequestBody UserRequest.UserProfileInfo userProfileInfo);
 }
