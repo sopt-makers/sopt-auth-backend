b225309cb4a53053e0ae438f366314817017983d
seoyeonjiin <duswls214@khu.ac.kr>
2025-07-11 13:34:12 +0900

    [FEAT] 전화번호 인증에 애플 심사 매직 넘버 로직 추가

--- src/main/java/sopt/makers/authentication/application/service/auth/VerifyVerificationService.java
+++ src/main/java/sopt/makers/authentication/application/service/auth/VerifyVerificationService.java
@@ -5,6 +5,7 @@ import static sopt.makers.authentication.domain.auth.exception.AuthFailure.INVAL
 
 import sopt.makers.authentication.application.port.in.auth.VerifyPhoneVerificationUsecase;
 import sopt.makers.authentication.application.port.out.auth.PhoneVerificationRepository;
+import sopt.makers.authentication.config.MagicLoginProperty;
 import sopt.makers.authentication.domain.auth.PhoneVerification;
 import sopt.makers.authentication.domain.auth.exception.AuthException;
 
@@ -17,10 +18,15 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VerifyVerificationService implements VerifyPhoneVerificationUsecase {
   private final PhoneVerificationRepository phoneVerificationRepository;
+  private final MagicLoginProperty magicLoginProperty;
 
   @Override
   @Transactional
   public VerifyVerificationResult verify(VerifyVerificationCommand command) {
+    if (isMagicVerification(command.phone(), command.code())) {
+      return new VerifyVerificationResult(magicLoginProperty.name(), magicLoginProperty.code());
+    }
+
     PhoneVerification targetVerification =
         PhoneVerification.of(command.phone(), command.verificationType(), command.code());
     PhoneVerification findVerification =
@@ -43,5 +49,16 @@ public class VerifyVerificationService implements VerifyPhoneVerificationUsecase
 
     return new VerifyVerificationResult(
         verifiedVerification.getName(), verifiedVerification.getPhone());
+  }
+
+  private boolean isMagicVerification(String phone, String code) {
+    if (!phone.equals(magicLoginProperty.phone())) {
+      return false;
+    }
+    if (!code.equals(magicLoginProperty.code())) {
+      throw new AuthException(INVALID_PHONE_VERIFICATION_CODE);
+    }
+
+    return true;
   }
 }
