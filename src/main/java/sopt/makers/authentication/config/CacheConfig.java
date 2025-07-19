faebd05b015b49847684c342fc8eca14a0876cc2
sung-silver <jinna0319@gmail.com>
2025-07-20 01:33:16 +0900

    [FEAT] getAllPresent 사용을 위한 CacheConfig 변경

--- src/main/java/sopt/makers/authentication/config/CacheConfig.java
+++ src/main/java/sopt/makers/authentication/config/CacheConfig.java
@@ -1,11 +1,29 @@
 package sopt.makers.authentication.config;
 
+import java.time.Duration;
+
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.extern.slf4j.Slf4j;
+import com.github.benmanes.caffeine.cache.Caffeine;
 
-@Slf4j
 @Configuration
 @EnableCaching
-public class CacheConfig {}
+public class CacheConfig {
+  private static final int EXPIRE_DAY = 1;
+  private static final int MAXIMUM_SIZE = 3000;
+  private static final String CACHE_NAME = "users";
+
+  @Bean
+  public CacheManager cacheManager() {
+    CaffeineCacheManager cacheManager = new CaffeineCacheManager(CACHE_NAME);
+    cacheManager.setCaffeine(
+        Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofDays(EXPIRE_DAY))
+            .maximumSize(MAXIMUM_SIZE));
+    return cacheManager;
+  }
+}
