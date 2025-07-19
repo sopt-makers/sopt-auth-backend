b787802b24d20fa7fa986027a79d3e45547f1afe
sung-silver <jinna0319@gmail.com>
2025-07-20 01:06:47 +0900

    [FEAT] CacheConfig 설정

--- src/main/java/sopt/makers/authentication/config/CacheConfig.java
+++ src/main/java/sopt/makers/authentication/config/CacheConfig.java
@@ -1,3 +1,11 @@
 package sopt.makers.authentication.config;
 
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableCaching
 public class CacheConfig {}
