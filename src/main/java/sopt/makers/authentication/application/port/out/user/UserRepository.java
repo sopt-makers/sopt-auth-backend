f7985dc25d040fd5810d614cc7cbaba73eca55c4
sung-silver <jinna0319@gmail.com>
2025-07-24 14:02:25 +0900

    [FEAT] UserRepository에 findByIdWithHistories 메서드 구현

--- src/main/java/sopt/makers/authentication/application/port/out/user/UserRepository.java
+++ src/main/java/sopt/makers/authentication/application/port/out/user/UserRepository.java
@@ -13,6 +13,8 @@ public interface UserRepository {
 
   User findById(Long userId);
 
+  User findByIdWithHistories(Long userId);
+
   User findByPhone(String phone);
 
   List<User> findAllById(List<Long> userIds);
