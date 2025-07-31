2af83525e0edb06db0f94e7658b7e97aaf475705
sung-silver <jinna0319@gmail.com>
2025-07-31 15:49:57 +0900

    [MOVE] CacheException 위치 이동

--- src/main/java/sopt/makers/authentication/adapter/out/cache/exception/CacheException.java
+++ src/main/java/sopt/makers/authentication/adapter/out/cache/exception/CacheException.java
@@ -1,4 +1,4 @@
-package sopt.makers.authentication.adapter.out.common.exception;
+package sopt.makers.authentication.adapter.out.cache.exception;
 
 import sopt.makers.authentication.common.code.FailureCode;
 import sopt.makers.authentication.common.exception.BaseException;
