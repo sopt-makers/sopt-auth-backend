774b5cab95f69673d31cbac73d84c44fbd03288f
seoyeonjiin <duswls214@khu.ac.kr>
2025-08-30 19:35:03 +0900

    [FEAT] 회원 탈퇴 성공 응답 코드 추가

--- src/main/java/sopt/makers/authentication/adapter/in/web/common/code/UserSuccess.java
+++ src/main/java/sopt/makers/authentication/adapter/in/web/common/code/UserSuccess.java
@@ -14,7 +14,10 @@ import lombok.RequiredArgsConstructor;
 public enum UserSuccess implements SuccessCode {
   GET_USER_PROFILE(HttpStatus.OK, "유저 기본 정보 조회에 성공했습니다."),
   UPDATE_USER_PROFILE(HttpStatus.OK, "유저 기본 정보 수정에 성공했습니다."),
-  GET_USER_COUNT(HttpStatus.OK, "유저 수 조회에 성공했습니다.");
+  GET_USER_COUNT(HttpStatus.OK, "유저 수 조회에 성공했습니다."),
+
+  // 204
+  WITHDRAW_USER(HttpStatus.NO_CONTENT, "회원 탈퇴에 성공했습니다. ");
 
   private final HttpStatus status;
   private final String message;
