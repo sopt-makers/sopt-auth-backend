13d57017e275b56ce679f79556553e1d8777fcfd
정윤 <sju08227@naver.com>
2025-09-16 18:17:02 +0900

    [CHORE] ApplicationConfig에 AppProperty 추가

--- src/main/java/sopt/makers/authentication/config/ApplicationConfig.java
+++ src/main/java/sopt/makers/authentication/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package sopt.makers.authentication.config;
 
+import sopt.makers.authentication.adapter.out.external.app.AppProperty;
 import sopt.makers.authentication.adapter.out.external.oauth.AppleOAuthProperty;
 import sopt.makers.authentication.adapter.out.external.oauth.GoogleOAuthProperty;
 import sopt.makers.authentication.adapter.out.external.oauth.MagicLoginProperty;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Configuration;
   AppleOAuthProperty.class,
   GoogleOAuthProperty.class,
   MagicLoginProperty.class,
-  PlaygroundProperty.class
+  PlaygroundProperty.class,
+  AppProperty.class
 })
 public class ApplicationConfig {}
