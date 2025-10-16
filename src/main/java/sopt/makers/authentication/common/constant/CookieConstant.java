1b05f610931d1f67ce517b0da5fcfdc929956309
sung-silver <jinna0319@gmail.com>
2025-10-16 20:33:24 +0900

    [FIX] WebConfig allowOrigins 범위 수정

--- src/main/java/sopt/makers/authentication/common/constant/CookieConstant.java
+++ src/main/java/sopt/makers/authentication/common/constant/CookieConstant.java
@@ -5,5 +5,7 @@ public final class CookieConstant {
 
   public static final String SAME_SITE_NONE = "None";
   public static final String COOKIE_DOMAIN = ".sopt.org";
-  public static final String CORS_ALLOWED_ORIGIN_PATTERN = "https://*.sopt.org";
+  public static final String[] CORS_ALLOWED_ORIGINS = {
+    "https://sopt-internal-dev.sopt.org", "https://playground.sopt.org"
+  };
 }
