1fcad1da083f3e654210b0488af2d6f26e0601df
sung-silver <jinna0319@gmail.com>
2026-01-18 00:13:47 +0900

    [DEL] 사용되지 않는 환경변수 정리

--- lambda/lambda-deploy-check-local.sh
+++ lambda/lambda-deploy-check-local.sh
@@ -146,10 +146,6 @@ deploy_with_sam() {
       MagicLoginPhone="${MAGIC_LOGIN_PHONE}" \
       MagicLoginCode="${MAGIC_LOGIN_CODE}" \
       MagicLoginName="${MAGIC_LOGIN_NAME}" \
-      PlaygroundExternalKey="${PLAYGROUND_EXTERNAL_KEY}" \
-      PlaygroundBaseUrl="${PLAYGROUND_BASE_URL}" \
-      AppExternalKey="${APP_EXTERNAL_KEY}" \
-      AppBaseUrl="${APP_BASE_URL}" \
     --region ${AWS_REGION} \
     --profile ${AWS_PROFILE}
 
